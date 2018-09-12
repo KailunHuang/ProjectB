1892a8215f09ecf2350ffdda1f2d4eaa06f0124f
shizhanx <43192138+shizhanx@users.noreply.github.com>
2018-09-12 11:46:24 +1000

    Add files via upload

--- swen30006/automail/CarefulRobot.java
+++ swen30006/automail/CarefulRobot.java
@@ -3,6 +3,7 @@ package automail;
 import java.util.LinkedList;
 
 import automail.Simulation.RobotType;
+import exceptions.FragileItemBrokenException;
 import strategies.IMailPool;
 import strategies.MyMailPool.Item;
 
@@ -12,6 +13,23 @@ public class CarefulRobot extends Robot {
 	public CarefulRobot(IMailDelivery delivery, IMailPool mailPool) {
 		super(delivery, mailPool, Simulation.RobotType.Careful);
 	}
+	
+	private void moveTowards(int destination) throws FragileItemBrokenException {
+        if (deliveryItem != null && deliveryItem.getFragile() || !tube.isEmpty() && tube.peek().getFragile()) throw new FragileItemBrokenException();
+        if (move) {
+	        if(current_floor < destination){
+	            current_floor++;
+	        }
+	        else{
+	            current_floor--;
+	        }
+	        move=false;
+        }
+        // Otherwise, move in the next round
+        else {
+            move = true;
+        }
+    }
 
 	public void fillStorageTube(LinkedList<Item> pool,LinkedList<Item> fragilePool,int lightCount) {
 		
